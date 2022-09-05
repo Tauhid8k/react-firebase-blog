2c5e4025dc398bf96ba6a0d0d6263f0953c1872d
Tauhid8k <tauhid8k@gmail.com>
2022-09-06 04:34:48 +0600

    responsive navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,59 @@
 import { Link } from 'react-router-dom';
-import { BiHome, BiEdit, BiUser, BiLogOutCircle } from 'react-icons/bi';
+import {
+  BiHome,
+  BiEdit,
+  BiUser,
+  BiLogOutCircle,
+  BiXCircle,
+} from 'react-icons/bi';
+import { useState } from 'react';
 
 const Navbar = ({ isAuth, userSignOut }) => {
+  const [isMenuActive, setIsMenuActive] = useState(false);
+
+  const handleMenuOpen = () => {
+    setIsMenuActive(!isMenuActive);
+  };
+
   return (
     <nav className='bg-white shadow py-3 mb-8'>
       <div className='container flex justify-between items-center'>
         <div className='logo font-medium text-2xl text-red-500'>
           <Link to='/'>FireBlog</Link>
         </div>
-        <ul className='menu flex gap-5 font-medium text-gray-700'>
+        <div className='hamburger' onClick={handleMenuOpen}>
+          <div className='line'></div>
+        </div>
+        <ul
+          className={`menu flex gap-5 font-medium text-gray-700 ${
+            isMenuActive ? 'menu-active' : ''
+          }`}
+        >
+          <div className='menu-close' onClick={handleMenuOpen}>
+            <BiXCircle />
+          </div>
           <li>
-            <Link to='/' className='flex items-center gap-1'>
+            <Link to='/' className='nav-link flex items-center gap-1'>
               <BiHome />
               Home
             </Link>
           </li>
           {isAuth && (
             <li>
-              <Link to='/create' className='flex items-center gap-1'>
+              <Link to='/create' className='nav-link flex items-center gap-1'>
                 <BiEdit /> Create
               </Link>
             </li>
           )}
           <li>
             {!isAuth ? (
-              <Link to='/login' className='flex items-center gap-1'>
+              <Link to='/login' className='nav-link flex items-center gap-1'>
                 <BiUser /> Login
               </Link>
             ) : (
               <button
                 type='button'
-                className='font-medium text-gray-700 flex items-center gap-1'
+                className='nav-link font-medium text-gray-700 flex items-center gap-1'
                 onClick={userSignOut}
               >
                 <BiLogOutCircle />
